b07d17333c713386ebfa755e04d4c3f2957b0adb
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2020-03-04 09:17:06 +0100

    Bugfix: check group availability correctly.

--- opm/simulators/wells/StandardWell_impl.hpp
+++ opm/simulators/wells/StandardWell_impl.hpp
@@ -1140,31 +1140,31 @@ namespace Opm
     StandardWell<TypeTag>::
     assembleGroupProductionControl(const Group& group, const WellState& well_state, const Opm::Schedule& schedule, const SummaryState& summaryState, EvalWell& control_eq, double efficiencyFactor, Opm::DeferredLogger& deferred_logger)
     {
-        if (!group.isAvailableForGroupControl()) {
-            // We cannot go any further up the hierarchy. This could
-            // be the FIELD group, or any group for which this has
-            // been set in GCONINJE or GCONPROD. If we are here
-            // anyway, it is likely that the deck set inconsistent
-            // requirements, such as GRUP control mode on a well with
-            // no appropriate controls defined on any of its
-            // containing groups. We will therefore use the wells' bhp
-            // limit equation as a fallback.
-            const auto& controls = well_ecl_.productionControls(summaryState);
-            control_eq = getBhp() - controls.bhp_limit;
-            return;
-        }
-
         const auto& well = well_ecl_;
         const auto pu = phaseUsage();
 
         const Group::ProductionCMode& currentGroupControl = well_state.currentProductionGroupControl(group.name());
         if (currentGroupControl == Group::ProductionCMode::FLD ||
             currentGroupControl == Group::ProductionCMode::NONE) {
-            // Produce share of parents control
-            const auto& parent = schedule.getGroup( group.parent(), current_step_ );
-            efficiencyFactor *= group.getGroupEfficiencyFactor();
-            assembleGroupProductionControl(parent, well_state, schedule, summaryState, control_eq, efficiencyFactor, deferred_logger);
-            return;
+            if (!group.isAvailableForGroupControl()) {
+                // We cannot go any further up the hierarchy. This could
+                // be the FIELD group, or any group for which this has
+                // been set in GCONINJE or GCONPROD. If we are here
+                // anyway, it is likely that the deck set inconsistent
+                // requirements, such as GRUP control mode on a well with
+                // no appropriate controls defined on any of its
+                // containing groups. We will therefore use the wells' bhp
+                // limit equation as a fallback.
+                const auto& controls = well_ecl_.productionControls(summaryState);
+                control_eq = getBhp() - controls.bhp_limit;
+                return;
+            } else {
+                // Produce share of parents control
+                const auto& parent = schedule.getGroup( group.parent(), current_step_ );
+                efficiencyFactor *= group.getGroupEfficiencyFactor();
+                assembleGroupProductionControl(parent, well_state, schedule, summaryState, control_eq, efficiencyFactor, deferred_logger);
+                return;
+            }
         }
 
         if (!group.isProductionGroup()) {
