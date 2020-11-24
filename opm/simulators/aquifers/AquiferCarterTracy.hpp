6548ee40d57e04bca5631e5bcde6d86e910a0c8d
Tor Harald Sandve <tosa@norceresearch.no>
2020-11-24 14:58:15 +0100

    dunify the aquifer models

--- opm/simulators/aquifers/AquiferCarterTracy.hpp
+++ opm/simulators/aquifers/AquiferCarterTracy.hpp
@@ -46,6 +46,7 @@ public:
     using typename Base::RateVector;
     using typename Base::Scalar;
     using typename Base::Simulator;
+    using typename Base::ElementMapper;
 
     using Base::waterCompIdx;
     using Base::waterPhaseIdx;
@@ -88,20 +89,15 @@ protected:
     // TODO: it is possible it should be a AD variable
     Scalar mu_w_; // water viscosity
 
-    // This function is used to initialize and calculate the alpha_i for each grid connection to the aquifer
     inline void initializeConnections() override
     {
         const auto& eclState = this->ebos_simulator_.vanguard().eclState();
-        const auto& ugrid = this->ebos_simulator_.vanguard().equilGrid();
         const auto& grid = eclState.getInputGrid();
 
         // We hack the cell depth values for now. We can actually get it from elementcontext pos
         this->cell_depth_.resize(this->size(), this->aquiferDepth());
         this->alphai_.resize(this->size(), 1.0);
         this->faceArea_connected_.resize(this->size(), 0.0);
-
-        auto cell2Faces = Opm::UgGridHelpers::cell2Faces(ugrid);
-        auto faceCells = Opm::UgGridHelpers::faceCells(ugrid);
 
         // Translate the C face tag into the enum used by opm-parser's TransMult class
         Opm::FaceDir::DirEnum faceDirection;
@@ -110,48 +106,69 @@ protected:
         Scalar denom_face_areas = 0.;
         this->cellToConnectionIdx_.resize(this->ebos_simulator_.gridView().size(/*codim=*/0), -1);
         for (size_t idx = 0; idx < this->size(); ++idx) {
-            const int cell_index = this->cartesian_to_compressed_.at(this->connections_[idx].global_index);
+            const auto global_index = this->connections_[idx].global_index;
+            const int cell_index = this->cartesian_to_compressed_.at(global_index);
             this->cellToConnectionIdx_[cell_index] = idx;
+            const auto cellCenter = grid.getCellCenter(global_index);
+            this->cell_depth_.at(idx) = cellCenter[2];
+        }
+        // get default areas for all intersections
+        const auto& gridView = this->ebos_simulator_.vanguard().gridView();
+        ElementMapper elemMapper(gridView, Dune::mcmgElementLayout());
+        auto elemIt = gridView.template begin</*codim=*/ 0>();
+        const auto& elemEndIt = gridView.template end</*codim=*/ 0>();
+        for (; elemIt != elemEndIt; ++elemIt) {
+            const auto& elem = *elemIt;
+            unsigned cell_index = elemMapper.index(elem);
+            int idx = this->cellToConnectionIdx_[cell_index];
 
-            const auto cellFacesRange = cell2Faces[cell_index];
-            for (auto cellFaceIter = cellFacesRange.begin(); cellFaceIter != cellFacesRange.end(); ++cellFaceIter) {
-                // The index of the face in the compressed grid
-                const int faceIdx = *cellFaceIter;
+            // only deal with connections given by the aquifer
+            if( idx < 0)
+                continue;
 
-                // the logically-Cartesian direction of the face
-                const int faceTag = Opm::UgGridHelpers::faceTag(ugrid, cellFaceIter);
+            auto isIt = gridView.ibegin(elem);
+            const auto& isEndIt = gridView.iend(elem);
+            for (; isIt != isEndIt; ++ isIt) {
+                // store intersection, this might be costly
+                const auto& intersection = *isIt;
 
-                switch (faceTag) {
+                // only deal with grid boundaries
+                if (!intersection.boundary())
+                    continue;
+
+                int insideFaceIdx  = intersection.indexInInside();
+                switch (insideFaceIdx) {
                 case 0:
                     faceDirection = Opm::FaceDir::XMinus;
                     break;
                 case 1:
                     faceDirection = Opm::FaceDir::XPlus;
                     break;
+
                 case 2:
                     faceDirection = Opm::FaceDir::YMinus;
                     break;
                 case 3:
                     faceDirection = Opm::FaceDir::YPlus;
                     break;
+
                 case 4:
                     faceDirection = Opm::FaceDir::ZMinus;
                     break;
                 case 5:
                     faceDirection = Opm::FaceDir::ZPlus;
                     break;
+
                 default:
                     OPM_THROW(Opm::NumericalIssue,
-                              "Initialization of Aquifer Carter Tracy problem. Make sure faceTag is correctly defined");
-                }
+                        "Initialization of Aquifer Carter Tracy problem. Make sure faceTag is correctly defined");                }
 
                 if (faceDirection == this->connections_[idx].face_dir) {
-                    this->faceArea_connected_.at(idx) = this->getFaceArea(faceCells, ugrid, faceIdx, idx);
+                    this->faceArea_connected_[idx] = this->getFaceArea(intersection, idx);
                     denom_face_areas += (this->connections_[idx].influx_mult * this->faceArea_connected_.at(idx));
+                    break;
                 }
             }
-            auto cellCenter = grid.getCellCenter(this->connections_[idx].global_index);
-            this->cell_depth_.at(idx) = cellCenter[2];
         }
 
         const double eps_sqrt = std::sqrt(std::numeric_limits<double>::epsilon());
